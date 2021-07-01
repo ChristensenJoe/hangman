001cbb48fd147dd09683fb8ad79f77ccfeed22e1
Joe Christensen <joechristensenwork@gmail.com>
2021-07-01 16:47:47 -0700

    Created basic layout and Word Box

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("Hello World!");
+const wordArray = ["Hello", "Goodbye", "Baby", "Steve", "Lucky"];
+const correctWord = randomWord(wordArray);
+const guessedLetters = [];
+
+document.addEventListener("DOMContentLoaded", () => {
+    let wordTable = document.querySelector("#wordTable");
+    correctWord.forEach((letter) => {
+        let letterSpace = document.createElement("th");
+        letterSpace.id = letter + "Space";
+        letterSpace.style.fontFamily = "Georgia, 'Times New Roman', Times, serif";
+        letterSpace.textContent = "_";
+        wordTable.appendChild(letterSpace);
+        console.log(letter);
+    });
+});
+
+/**
+ * 
+ * @param arr an Array of Strings
+ * @returns an array of characters created from one of the argument's elements
+ */
+function randomWord(arr) {
+    return arr[Math.floor(Math.random() * wordArray.length)].toUpperCase().split("");
+}

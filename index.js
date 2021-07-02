e12231384d522f463954bcbe5cfa5e5cc3a1d13f
Joe Christensen <joechristensenwork@gmail.com>
2021-07-01 20:10:35 -0700

    Finished Formatting and setting up listeners for Letters

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const wordArray = ["Hello", "Goodbye", "Baby", "Steve", "Lucky"];
 const correctWord = randomWord(wordArray);
 const guessedLetters = [];
+const letterArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
 document.addEventListener("DOMContentLoaded", () => {
     let wordTable = document.querySelector("#wordTable");
     correctWord.forEach((letter) => {
         let letterSpace = document.createElement("th");
+        letterSpace.className = "wordClass";
         letterSpace.id = letter + "Space";
         letterSpace.style.fontFamily = "Georgia, 'Times New Roman', Times, serif";
         letterSpace.textContent = "_";
         wordTable.appendChild(letterSpace);
         console.log(letter);
+    });
+
+    letterArray.forEach((letter) => {
+        let letterCell = document.querySelector("#" +letter);
+        letterCell.addEventListener("click", () => {
+            if(correctWord.includes(letter)) {
+            }
+        });
     });
 });
 
